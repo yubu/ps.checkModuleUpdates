ba75c8d732a3c6ff6d3a4965bc9ea81140f74b09
yubu <yubu.git@gmail.com>
2019-11-26 15:31:21 +0200

    0.0.3

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # ps.checkModuleUpdates
 Powershell module to check and install module updates. Will also create scheduled task which will run weekly and send toast notification with list of outdated modules.
 
+### Default scopes
+- For Windows Powershell: AllUsers
+- For Powershell Core: CurrentUser
+- It could be changed by -Scope and/or -ScopeCore parameters
+
 ### Installation from Github
 ```powershell
 cd $env:Userprofile\Documents\WindowsPowerShell\Modules\
@@ -25,16 +30,33 @@ Install-Module ps.checkModuleUpdates
 gcm -module ps.checkModuleUpdates
 help -ex Check-ModuleUpdates
 ```
+
 ##### Examples
 ```powershell
 Check-ModuleUpdates -all -sendToast
-Check-ModuleUpdates PSReadline -all -sendToast -allowPrerelease -skipUpdate "posh-git|ps.checkModuleUpdates"
+Check-ModuleUpdates -all -sendToast -allowPrerelease -skipUpdate "posh-git|ps.checkModuleUpdates"
+Check-ModuleUpdates "posh-git|ps.checkModuleUpdates" -sendToast -allowPrerelease
 Check-ModuleUpdates -createSchedTask
+Check-ModuleUpdates -createSchedTaskWithTranscript
+
+cmu "posh-git|ps.checkModuleUpdates" -update -allowPrerelease -skipScan
+"posh-git|ps.checkModuleUpdates" | cmu -allowPrerelease -update -skipScan
+Will install/update modules right away with no scanning their installed versions
+
+cmu -compressTranscriptDir 
+Will save the space by set the compression attribute to log/transcript directory ($env:USERPROFILE\.ps.checkModuleUpdate)
 ```
 
 ##### Scheduled task
 ```powershell
 cmu -createSchedTask
+Will create scheduled task to run the script every Friday at 5am. SchedTask will be created for powershell edition, the command was ran from
+
+cmu -createSchedTaskWithTranscript
+Will create scheduled task with transcript enabled
+
+cmu -getLastUpdateCommand | iex
+Retrieve and execute the update command from the transcript file of latest scheduled task run
 ```
 When scheduled task is created, it will send the [toast notification](https://blogs.msdn.microsoft.com/tiles_and_toasts/2015/07/08/toast-notification-and-action-center-overview-for-windows-10/) with list of outdated modules.
 This notification will disappear after few seconds. To make it's appearance permanent in the action center (WinKey+A) you'll need to do the following:
@@ -45,9 +67,10 @@ This notification will disappear after few seconds. To make it's appearance perm
 5. Find Windows Powershell and/or Powershell 6 (x64)
 6. Click on it and enable "Notifications" and "Show notification in action center"
 
-The toast notification will stay in the action center (WinKey+A) until it will be manually dismissed.
+The toast notification will stay in the action center (WinKey+A) until it will be manually dismissed
 
 Then you can run the update command on modules of your choice:
 ```powershell
-cmu -allowPrerelease -all -update "module|module|module"
+cmu -allowPrerelease -all -update "module1|module2|module3"
+cmu -getLastUpdateCommand | iex
 ```
--- ps.checkModuleUpdates.psm1
+++ ps.checkModuleUpdates.psm1
@@ -17,11 +17,29 @@ function Get-ModuleUpdates {
 		Check-ModuleUpdates -update -skipUpdate "PSReadLine"
 		Will update all imported modules except module PSReadLine
 	.Example
+		"scour|PSWindowsUpdate|Pscx" | cmu -allowPrerelease -update -skipScan
+		Will update modules "scour|PSWindowsUpdate|Pscx" with no scan of installed module versions
+	.Example
+		cmu "scour|PSWindowsUpdate|Pscx" -allowPrerelease -update -skipScan
+		Will update modules "scour|PSWindowsUpdate|Pscx" with no scan of installed module versions
+	.Example
 		Check-ModuleUpdates PackageManagement -all -update
 		Will check and update module PackageManagement
 	.Example
 		Check-ModuleUpdates -all -update
 		Will try to update all available modules
+	.Example
+		Check-ModuleUpdates -createSchedTask
+		Will create scheduled task to run the script every Friday at 5am. SchedTask will be created for powershell edition, the command was ran from
+	.Example
+		cmu -createSchedTaskWithTranscript
+		Will create scheduled task with transcript enabled
+	.Example
+		cmu -getLastUpdateCommand
+		Get latest module update command from the transcript file, created by scheduled task
+	.Example
+		cmu -getLastUpdateCommand | iex
+		Run latest module update command 
 	.Example
 		Check-ModuleUpdates -all -update -skipUpdate "PSReadLine|PSWindowsUpdate"
 		Will update all modules available except modules PSReadLine and PSWindowsUpdate
@@ -38,12 +56,21 @@ function Get-ModuleUpdates {
 		Check-ModuleUpdates -all -sendToast
 		Will check all modules for updates and send toast notification to Action center
 	.Example
-		Check-ModuleUpdates -createSchedTask
-		Will create scheduled task to run the script every Friday at 5am. SchedTask will be created for powershell edition, the command was ran from
+		cmu -compressTranscriptDir 
+		Will set compression attribute to log/transcript directory ($env:USERPROFILE\.ps.checkModuleUpdate)
 	#>
 	[CmdletBinding()]
 	[Alias("cmu","Check-ModuleUpdates")]
-	param([switch]$update,[switch]$all,[switch]$sendToast,[switch]$createSchedTask,$module="",$skipUpdate="",$schedTaskScriptPath="",[switch]$allowPrerelease)
+	param (
+		[Parameter(Mandatory=$False,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Position=0)][string]$module="",
+		[Parameter(Mandatory=$False,ValueFromPipeline=$False,ValueFromPipelineByPropertyName=$False)][string]$skipUpdate="",
+		[Parameter(Mandatory=$False,ValueFromPipeline=$False,ValueFromPipelineByPropertyName=$False)][string]$schedTaskScriptPath="",
+		[Parameter(Mandatory=$False,ValueFromPipeline=$False,ValueFromPipelineByPropertyName=$False)][string]$schedTaskTranscriptPath="$env:USERPROFILE\.ps.checkModuleUpdate\Transcripts",
+		[Parameter(Mandatory=$False,ValueFromPipeline=$False,ValueFromPipelineByPropertyName=$False)][string][ValidateSet("AllUsers","CurrentUser")]$Scope="AllUsers",
+		[Parameter(Mandatory=$False,ValueFromPipeline=$False,ValueFromPipelineByPropertyName=$False)][string][ValidateSet("AllUsers","CurrentUser")]$ScopeCore="CurrentUser",
+		[switch]$update, [switch]$all, [switch]$sendToast, [switch]$createSchedTask, [switch]$createSchedTaskWithTranscript, [switch]$allowPrerelease, [switch]$skipScan, [switch]$getLastUpdateCommand,
+		[switch]$compressTranscriptDir
+	)
 	
 	begin {
 		$exclude="excludePermanetSomethingIfNeeded"
@@ -56,9 +83,24 @@ function Get-ModuleUpdates {
 		$ToastXml=[Windows.Data.Xml.Dom.XmlDocument]::new()
 		# Get-StartApps  | sort name
 		$appID='{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
+
+		# Default scope according platform
+		# https://blogs.msmvps.com/richardsiddaway/2019/09/29/receive-job-keep-parameter/
+		if ($PSVersionTable.PSEdition -eq "Core") {$PSDefaultParameterValues=@{'Install-module:Scope'="$ScopeCore";'Update-Module:Scope'="$ScopeCore"}}
+		else {$PSDefaultParameterValues=@{'Install-module:Scope'="$Scope";'Update-Module:Scope'="$Scope"}}
+
+		if ($getLastUpdateCommand) {
+			try {dir $schedTaskTranscriptPath\*$($PSVersionTable.PSEdition)* | sort LastWriteTime -desc | select -first 1 | gc | sls "cmu -update -all -allowPrerelease -skipScan"; break}
+			catch {$_.Exception}
+		}
+
+		# compact.exe /s /c $env:USERPROFILE\.ps.checkModuleUpdate\
+		if ($compressTranscriptDir) {compact.exe /s /c $env:USERPROFILE\.ps.checkModuleUpdate\}
 	}
 
 	Process {
+		if (!$module -and $skipScan) {Write-Host "ERROR: No module names provided for update. Run command with no -skipScan!" -f Red; return}
+
 		# Create sched task
 		if ($createSchedTask) {
 			# $actionArgCommandString="-command . $SchedTaskScriptPath; Check-ModuleUpdates -all -sendToast"
@@ -88,8 +130,42 @@ function Get-ModuleUpdates {
 			Register-ScheduledTask @splatArgs
 			return
 		}
-		
-		if ($all) {$gModParam=@{ListAvailable=$true}} else {$gModParam=@{ListAvailable=$false}}
+
+		if ($createSchedTaskWithTranscript) {
+			
+			$schedTaskTranscriptPathFile="Transcript-$($PSVersionTable.PSEdition)-"+'$(date -f yyyy-MM-dd)-$(Get-Random 1000).txt'
+			$actionArgCommandString="-command import-module ps.checkModuleUpdates; if (!(Test-Path $schedTaskTranscriptPath)) {mkdir -force $schedTaskTranscriptPath}; Start-Transcript -Path $schedTaskTranscriptPath\$schedTaskTranscriptPathFile; Check-ModuleUpdates -all -sendToast; Stop-Transcript"
+			if ($PSVersionTable.PSEdition -eq "Core") {
+				$splatNewSchT=@{
+					Execute="pwsh"
+					Argument=$actionArgCommandString
+				}
+				$splatArgs=@{
+					Action=New-ScheduledTaskAction @splatNewSchT
+					Trigger=New-ScheduledTaskTrigger -Weekly -DaysOfWeek Friday -At 5am
+					TaskName="RunModuleUpdateScriptCoreWithTranscript"
+				}
+			}
+			elseif ($PSVersionTable.PSEdition -eq "Desktop") {
+				$splatNewSchT=@{
+					Execute="powershell"
+					Argument=$actionArgCommandString
+				}
+				$splatArgs=@{
+					Action=New-ScheduledTaskAction @splatNewSchT
+					Trigger=New-ScheduledTaskTrigger -Weekly -DaysOfWeek Friday -At 5am
+					TaskName="RunModuleUpdateScriptWithTranscript"
+				}
+			}
+			Register-ScheduledTask @splatArgs
+			return
+
+		}
+
+		if (!$skipScan) {
+
+			if ($all) {$gModParam=@{ListAvailable=$true}} else {$gModParam=@{ListAvailable=$false}}
+			
 			if ($allowPrerelease) { 
 				get-module @gModParam | ? name -notmatch "$exclude" | ? name -match "$module" | select -Unique -pv localModule | %{
 					"--> $_ --> $($_.author)", $(
@@ -107,16 +183,22 @@ function Get-ModuleUpdates {
 						)
 				} 
 			}
-			else { get-module @gModParam | ? name -notmatch "$exclude" | ? name -match "$module" | select -Unique -pv localModule | %{"--> $_ --> $($_.author)", $_.version.ToString(), (find-module -name $_ -ea silent | %{if ([version]$_.version -gt $localModule.version) {$_.version + ' <--'; [array]$changelist+=$localModule.name} else {($_.version).tostring()} })} }
+			else { get-module @gModParam | ? name -notmatch "$exclude" | ? name -match "$module" | select -Unique -pv localModule | %{"--> $_ --> $($_.author)", $_.version.ToString(), (find-module -name $_ -ea silent | %{if ([version]$_.version -gt $localModule.version) {[string]$_.version + ' <--'; [array]$changelist+=$localModule.name} else {($_.version).tostring()} })} }
 		
-		$changelist=$changelist | ?{$_}
-		if ($skipUpdate) {$changelist=$changelist | ?{$_ -notmatch "$skipUpdate"} | ?{$_}}
 
-		if (!$changelist -and !$skipUpdate) {write-host "All is up to date." -f green}
-		elseif (!$changelist -and $skipUpdate) {write-host "All is up to date." -f green; write-host "Module(s) skipped: " -f green -nonewline; write-host "$skipUpdate" -f yellow}
-		elseif ($changelist -and $skipUpdate) {Write-Host "Module(s) to update: " -f yellow -nonewline; Write-Host "$changelist" -f red; write-host "Module(s) skipped: " -f green -nonewline; write-host "$skipUpdate" -f yellow}
-		elseif ($update -and $skipUpdate -and !$changelist) {write-host "All is up to date." -f green; write-host "Module(s) skipped: " -f green -nonewline; write-host "$skipUpdate" -f yellow}
-		else {Write-Host "Module(s) to update: " -f yellow -nonewline; Write-Host "$changelist" -f red}
+			$changelist=$changelist | ?{$_}
+			if ($skipUpdate) {$changelist=$changelist | ?{$_ -notmatch "$skipUpdate"} | ?{$_}}
+
+			if (!$changelist -and !$skipUpdate) {write-host "All is up to date." -f green}
+			elseif (!$changelist -and $skipUpdate) {write-host "All is up to date." -f green; write-host "Module(s) skipped: " -f green -nonewline; write-host "$skipUpdate" -f yellow}
+			elseif ($changelist -and $skipUpdate) {Write-Host "Module(s) to update: " -f yellow -nonewline; Write-Host "$changelist" -f red; write-host "Module(s) skipped: " -f green -nonewline; write-host "$skipUpdate" -f yellow}
+			elseif ($update -and $skipUpdate -and !$changelist) {write-host "All is up to date." -f green; write-host "Module(s) skipped: " -f green -nonewline; write-host "$skipUpdate" -f yellow}
+			else {Write-Host "Module(s) to update: " -f yellow -nonewline; Write-Host "$changelist" -f red; Write-Host "Run: " -nonewline -f yellow; Write-Host "`"$($changelist -join "|")`" | cmu -update -all -allowPrerelease -skipScan`n" -f cyan}
+
+		}
+		
+		if ($module -and $skipScan) {[array]$changelist=($module).split('|')}
+		
 		if ($update -and $changelist -and $allowPrerelease) {
 			Write-Output "`nWill update: $changelist ..."; foreach ($modName in $changelist) {install-module $modName -force -allowClobber -AllowPrerelease}
 		}
